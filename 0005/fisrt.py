a72ddd0ffffc4ed1842c06cdf362be2c62a68e1f
lixiwang <404174262@qq.com>
2016-08-18 21:21:03 +0800

    add 0004

---
+++ 0005/fisrt.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # 找到字符串的中间值,仅仅为了更快找到回文
+        mid = len(str) // 2
+        #默认回文长度为0
+        length = 0
+        #回文分两种：一种为bbabb,一种为abba
+        #第一种：
+        if
+
+
+# str = 'lixiwang'
+# a = len(str) // 2
+# b = str.find('4')
+# print(a)
+# print(b)
