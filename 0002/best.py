ebfb50b7af5f310a04af4d249b8555ea10710c35
lixiwang <404174262@qq.com>
2016-08-18 18:42:22 +0800

    first

---
+++ 0002/best.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        head = previous = None
+        while l1 or l2 or carry:
+            s = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            carry, val = s / 10, s % 10
+            node = ListNode(val)
+            if previous:
+                previous.next = node
+            else:
+                head = node
+            previous = node
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return head
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+####第 0001 题
+		Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+		给定一个整数数组，找出其中两个数满足相加等于你指定的目标数字。
+			
+			Example:
+			Given nums = [2, 7, 11, 15], target = 9,
+			Because nums[0] + nums[1] = 2 + 7 = 9,
+			return [0, 1].
+####第 0002 题	
+		You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+			Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+			Output: 7 -> 0 -> 8
+			
