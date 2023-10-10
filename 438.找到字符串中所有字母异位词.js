5fe0c41562b1f44d011aeb545d4e0cbc42bccf2f
bhwa233 <404174262@qq.com>
2023-10-10 23:00:02 +0800

    commit 2023年10月10日 23:00:02

--- 438.找到字符串中所有字母异位词.js
+++ 438.找到字符串中所有字母异位词.js
@@ -11,24 +11,24 @@
  * @return {number[]}
  */
 var findAnagrams = function (s, p) {
-  const res = []
-  const map = {}
-  const currentMap = {}
+  const res = [];
+  const map = {};
+  const currentMap = {};
   for (let char of p) {
-    map[char] = (map?.[char] ?? 0) + 1
-    currentMap[char] = 0
+    map[char] = (map[char] ?? 0) + 1;
+    currentMap[char] = 0;
   }
-  let left = 0, right = 0
+  let left = 0, right = 0;
   const match = () => {
     for (let key in currentMap) {
-      if (currentMap[key] !== map[key] ?? 0) {
-        return false
+      if (currentMap[key] !== (map[key] ?? 0)) {
+        return false;
       }
     }
-    return true
-  }
+    return true;
+  };
   while (right < s.length) {
-    const rightChar = s[right]
+    const rightChar = s[right];
     if (right - left + 1 <= p.length) {
       currentMap[rightChar] = (currentMap[rightChar] ?? 0) + 1;
       right++;
@@ -37,10 +37,10 @@ var findAnagrams = function (s, p) {
       left++;
     }
     if (match()) {
-      res.push(left)
+      res.push(left);
     }
   }
-  return res
+  return res;
 };
 // @lc code=end
 
